b4d489992786b02e03659d58ac8f8ffe1786ef0e
vladi <vladi@VLADISPAS>
2023-10-19 17:53:31 +0300

    Add project files.

---
+++ Ispas_Vlad-Traian_Lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ispas_Vlad_Traian_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
